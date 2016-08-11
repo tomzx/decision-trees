e31d5d9e46541a51640cc67e426ec0b76d75ed95
Tom Rochette <roctom@gmail.com>
2016-08-11 15:07:54 -0400

    Add the ability to trace a person state using the command line argument "--trace-person".

--- person.py
+++ person.py
@@ -1,5 +1,8 @@
 class Person(object):
 	def __init__(self):
 		self.location = None
-		self.clothes = None
+		self.clothes = set()
 		self.day = None
+
+	def __str__(self):
+		return self.__class__.__name__ + "(" + str(self.__dict__) + ")"
