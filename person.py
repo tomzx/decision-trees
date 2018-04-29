2b025529067ff5bd0e8dc9c128a8d0c671ec2a37
Tom Rochette <roctom@gmail.com>
2018-04-29 00:00:25 -0400

    Add the ability for a person to remember and recall facts.

--- person.py
+++ person.py
@@ -5,6 +5,16 @@ class Person(object):
 		self.day = None
 		self.time = None
 		self.day_type = None
+		self.memory = {} # facts stored in memory
+
+	def recall(self, fact):
+		value = self.memory.get(fact)
+		print('Recalls {} is {}'.format(fact, value))
+		return value
+
+	def remember(self, fact, value=True):
+		print('Will remember {} is {}'.format(fact, value))
+		self.memory[fact] = value
 
 	def __str__(self):
 		return self.__class__.__name__ + "(" + str(self.__dict__) + ")"
