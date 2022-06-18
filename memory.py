aac60e1f53d4a75c56b9af394b6662c30af113bf
Tom Rochette <roctom@gmail.com>
2022-06-18 12:31:45 -0400

    Move day, time, day type to memory

--- memory.py
+++ memory.py
@@ -1,6 +1,10 @@
+Day = "day"
+DayType = "day type"
+Time = "time"
+
 class Memory:
     def __init__(self):
-        self.memory = {}
+        self.__dict__["memory"] = {}
 
     def recall(self, fact: str) -> str:
         value = self.memory.get(fact)
@@ -10,3 +14,9 @@ class Memory:
     def remember(self, fact: str, value: bool = True) -> None:
         print(f'Will remember {fact} is {value}')
         self.memory[fact] = value
+
+    def __getattr__(self, fact: str) -> str:
+        return self.recall(fact)
+
+    def __setattr__(self, fact: str, value: str) -> None:
+        self.remember(fact, value)
--- person.py
+++ person.py
@@ -6,9 +6,6 @@ class Person(object):
 	def __init__(self):
 		self.location = None
 		self.clothes = set()
-		self.day = None
-		self.time = None
-		self.day_type = None
 		self.memory = Memory()
 		self.items = ItemCollection()
 
